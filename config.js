032c99c13375d939848aef2d046b431dfa6bb8df
boris <boris.karastanev@progress.com>
2017-06-23 14:42:28 +0300

    Updated config files to match caching update
    Signed-off-by: boris <boris.karastanev@progress.com>

---
+++ config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Config = {};
+
+Config.options = {
+    cacheTime: 600 // Value in sec
+};
+
+Config.appTemplates = [
+    'template-drawer-navigation-ng',
+    'template-drawer-navigation-ts',
+    'template-drawer-navigation',
+    'template-tab-navigation',
+    'template-tab-navigation-ts',
+    'template-tab-navigation-ng',
+    'template-master-detail',
+    'template-master-detail-ng',
+    'template-master-detail-ts',
+    'template-blank-ts',
+    'template-blank',
+    'template-blank-ng'
+];
+
+module.exports = Config;
